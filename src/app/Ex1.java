21b7b6b2d7b5325436004e97266b2b8e036cbe21
Nestor162 <nestorcicardini@gmail.com>
2023-05-10 14:50:49 +0200

    Svolto esercizio 1

---
+++ src/app/Ex1.java
@@ -0,0 +1,15 @@
+package app;
+
+import threads.Thread1;
+import threads.Thread2;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Thread1 t1 = new Thread1();
+		Thread2 t2 = new Thread2();
+		t1.start();
+		t2.start();
+	}
+
+}
---
+++ src/threads/Thread2.java
@@ -0,0 +1,18 @@
+package threads;
+
+public class Thread2 extends Thread {
+
+	@Override
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("#");
+		}
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
