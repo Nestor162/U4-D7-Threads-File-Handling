7bea8a517202e075b06b0674b3675fb754396dad
Nestor162 <nestorcicardini@gmail.com>
2023-05-10 17:27:49 +0200

    Migliorato esercizio 1

--- src/app/Ex1.java
+++ src/app/Ex1.java
@@ -1,13 +1,12 @@
 package app;
 
 import threads.Thread1;
-import threads.Thread2;
 
 public class Ex1 {
 
 	public static void main(String[] args) {
-		Thread1 t1 = new Thread1();
-		Thread2 t2 = new Thread2();
+		Thread1 t1 = new Thread1('*');
+		Thread1 t2 = new Thread1('#');
 		t1.start();
 		t2.start();
 	}
--- src/threads/Thread1.java
+++ src/threads/Thread1.java
@@ -1,10 +1,18 @@
 package threads;
 
 public class Thread1 extends Thread {
+
+	char symbol;
+
+	public Thread1(char symbol) {
+		super();
+		this.symbol = symbol;
+	}
+
 	@Override
 	public void run() {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("*");
+			System.out.println(symbol);
 
 			try {
 				Thread.sleep(1000);
