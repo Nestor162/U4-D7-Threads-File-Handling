7395c8558384bd19ce2079ca40b5f3a468eb3860
Nestor162 <nestorcicardini@gmail.com>
2023-05-10 15:47:41 +0200

    Svolto esercizio 2

---
+++ src/app/Ex2.java
@@ -0,0 +1,60 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import threads.ThreadArray1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		List<Integer> randomNumbers = new ArrayList<>();
+
+		for (int i = 0; i < 3000; i++) {
+			randomNumbers.add((int) Math.floor(Math.random() * 100));
+		}
+
+		List<Integer> part1 = randomNumbers.subList(0, 999);
+		ThreadArray1 t1 = new ThreadArray1(part1);
+
+		List<Integer> part2 = randomNumbers.subList(999, 1999);
+		ThreadArray1 t2 = new ThreadArray1(part2);
+
+		List<Integer> part3 = randomNumbers.subList(2000, 3000);
+		ThreadArray1 t3 = new ThreadArray1(part3);
+
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		t2.start();
+		try {
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		t3.start();
+		try {
+			t3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		int t1Result = t1.getSum();
+		int t2Result = t2.getSum();
+		int t3Result = t3.getSum();
+
+		System.out.println("Prima parte: \t" + t1Result);
+		System.out.println("Seconda parte: \t" + t2Result);
+		System.out.println("Terza parte: \t" + t3Result);
+
+		System.out.println("\nTotale: " + (t1Result + t2Result + t3Result));
+	}
+
+}
---
+++ src/threads/ThreadArray1.java
@@ -0,0 +1,38 @@
+package threads;
+
+import java.util.List;
+
+public class ThreadArray1 extends Thread {
+
+	private List<Integer> array;
+	private int sum;
+
+	public ThreadArray1(List<Integer> array) {
+		super();
+		this.array = array;
+		this.sum = 0;
+	}
+
+	public List<Integer> getArray() {
+		return array;
+	}
+
+	public void setArray(List<Integer> array) {
+		this.array = array;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+
+	@Override
+	public void run() {
+
+		for (int i = 0; i < array.size(); i++)
+			sum += array.get(i);
+	}
+}
